121e39f7564a957cf055cff690713e83fd35acce
bdomingu <beatrizdominguez14@gmail.com>
2023-02-15 09:44:13 -0700

    Added more styling

--- src/components/CompleteTasks.js
+++ src/components/CompleteTasks.js
@@ -4,14 +4,18 @@ function CompleteTasks({completedTasks, deleteTasks}) {
     
   return (
     <div className='todo-container'>
-        <h1>Completed Tasks</h1>
+        <h1>Done</h1>
         <div className='task-container'>
           {completedTasks?.map((completed) => {
             return(
             <>
            
             <li key={completed._id}>{completed.title}</li>
-            <button onClick={() => deleteTasks(completed._id)}>Delete</button>
+            <button
+            className='delete-button'
+            onClick={() => deleteTasks(completed._id)}>
+            <span className='fa-solid fa-trash-can'></span>
+            </button>
          
             </>
             )
--- src/components/IncompleteTasks.js
+++ src/components/IncompleteTasks.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fatrash } from '@fortawesome/free-solid-svg-icons';
 
 function IncompleteTasks({tasks, deleteTasks, markComplete, handleTaskSelection}) {
 
   return (
     <div className='todo-container'>
-      <h1>Incomplete Tasks</h1>
-      <button onClick={() => markComplete()}>Complete</button>
+      <h1>To-do's</h1>
+      <button className='complete-button' onClick={() => markComplete()}>Complete</button>
       <div className='task-container'>
       {tasks.map((task) => {
         return (
-            <><li key={task._id}><input type="checkbox" onChange={(e) => handleTaskSelection(e, task)} />
-            {task.title}</li><button onClick={() => deleteTasks(task._id)}>Delete</button></>
+            <><li key={task._id}>
+            <input 
+            type="checkbox" 
+            className='checkbox'
+            onChange={(e) => handleTaskSelection(e, task)} />
+            {task.title}</li>
+            <button className='delete-button' onClick={() => deleteTasks(task._id)}>
+            <span className='fa-solid fa-trash-can'></span>
+            </button></>
         )
 
       })}
